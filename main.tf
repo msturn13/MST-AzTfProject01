f8b6bda1bafb15f282e1fd9661b4629687b78f75
mturnbull012 <49628708+mturnbull012@users.noreply.github.com>
2021-11-14 19:23:47 -0600

    Updated subnet variables and nic build
    updated the subnet variables and added node_count variable and variable for nic.

--- main.tf
+++ main.tf
@@ -17,11 +17,13 @@
    }
  }
 
+#Create Resource Group
  resource "azurerm_resource_group" "rg" {
   name     = "mst-cus-rg"
   location = var.region
 }
 
+#Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
     name    = "mst-cus-vnet01"
     resource_group_name =   azurerm_resource_group.rg.name
@@ -30,10 +32,26 @@ resource "azurerm_virtual_network" "vnet" {
 
 }
 
+#vNet subnets 
 resource "azurerm_subnet" "test_subnet" {
     name = "${lookup(element(var.subnet_prefix, count.index), "name")}"
     count = "${length(var.subnet_prefix)}"
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefix = "${lookup(element(var.subnet_prefix, count.index), "ip")}"
+}
+
+#Create Availability Set
+resource "azurerm_availability_set" "aset" {
+  name                = "mst-cus-aset01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create Network Interface
+resource "azurerm_network_interface" "nic" {
+    count = var.node_count
+    name = each.value
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
 }
--- variables.tf
+++ variables.tf
@@ -8,19 +8,31 @@ variable "subnet_prefix" {
   default = [
     {
       ip      = "10.0.0.0/24"
-      name     = "subnet-1"
+      name     = "mst-cus-vnet01-sn01"
     },
     {
       ip      = "10.0.1.0/24"
-      name     = "subnet-2"
+      name     = "mst-cus-vnet01-sn02"
     },
     {
       ip      = "10.0.2.0/24"
-      name     = "subnet-1"
+      name     = "mst-cus-vnet01-sn03"
     },
     {
       ip      = "10.0.3.0/24"
-      name     = "subnet-1"
+      name     = "mst-cus-vnet01-sn04"
     }
    ]
 }
+
+variable "node_count" {
+type = number
+default = 2
+}
+
+variable "nic" {
+  type        = list(string)
+  default     = ["vm-nic01","vm-nic02"]
+
+}
+
